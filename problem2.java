25ca2275588e76068370676ad908591d890d9b8e
Hemamadhuri265 <162794993+Hemamadhuri265@users.noreply.github.com>
2025-05-20 16:13:02 +0530

    Create problem2.java

---
+++ problem2.java
@@ -0,0 +1,32 @@
+package problems;
+import java.util.Scanner;
+public class Problem2 {
+	public static void main(String args[]) {
+		
+			Scanner sc = new Scanner(System.in);
+			
+			char again;
+			 do {
+		            System.out.println("\nEnter your Number to generate Odd series: ");
+		            int a = sc.nextInt();
+		            boolean firstnum = true;
+
+		            for (int i = 0; i <= a; ++i) {
+		                if (i % 2 != 0) {
+		                    if (!firstnum) {
+		                        System.out.print(", ");
+		                    }
+		                    System.out.print(i);
+		                    firstnum = false;
+		                }
+		            }
+
+		            System.out.println("\n\nDo you want to enter another number? (Y/N)");
+		            again = sc.next().charAt(0);
+
+		        } while (again == 'Y' || again == 'y');
+
+		        System.out.println("Program exited.");
+		        sc.close();
+		    }
+		}
